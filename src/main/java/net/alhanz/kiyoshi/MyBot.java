a2c56fd473cdcbcf6004400b435bbd8ed0c134ea
alhanz <alhanz1135@gmail.com>
2024-05-08 15:00:22 +0800

    Added Discord Rich Presence (Temporary)

--- src/main/java/net/alhanz/kiyoshi/MyBot.java
+++ src/main/java/net/alhanz/kiyoshi/MyBot.java
@@ -2,15 +2,15 @@ package net.alhanz.kiyoshi;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.intent.Intent;
+import org.javacord.api.entity.activity.ActivityType;
 
 public class MyBot {
     public static void main(String[] args) {
         DiscordApi api = new DiscordApiBuilder()
-                .addIntents(Intent.GUILD_PRESENCES)
                 .setToken(SecKey.tokenKey())
                 .login()
                 .join();
+        api.updateActivity(ActivityType.WATCHING, "Hentai");
 
     }
 }
