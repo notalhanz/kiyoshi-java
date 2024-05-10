dce03491b736f8a3786558b279c92df44ba747ea
alhanz <alhanz1135@gmail.com>
2024-05-10 20:02:08 +0800

    Added visualBar() for cpu and memory (WIP) usage.

--- src/main/java/net/alhanz/kiyoshi/System.java
+++ src/main/java/net/alhanz/kiyoshi/System.java
@@ -20,44 +20,53 @@ public class System implements MessageCreateListener {
     String pBar75 = "<:bar75:1238447786127654942>";
     String pBarFull = "<:bar100:1238447783514603581>";
 
+    public String visualBar(double valuePercent) {
+
+        // Bar is composed of 10 bar emojis.
+        int fullBar = (int) valuePercent / 10;
+
+        // Declaring variables for Remainder.
+        int pBar25Count = 0, pBarHalfCount = 0, pBar75Count = 0;
+        double remainder = (valuePercent / 10) % 1;
+
+        // Checking Remainder for the following split bars.
+        if (remainder >= .75) {
+            ++pBar75Count;
+        } else if (remainder >= .5) {
+            ++pBarHalfCount;
+        } else if (remainder >= .25) {
+            ++pBar25Count;
+        }
+
+        // Calculate for bar to generate each of the bars.
+        int remaining = 10 - fullBar - pBar25Count - pBarHalfCount - pBar75Count;
+
+        // Formatting the value
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        return "\n[" +
+                pBarFull.repeat(fullBar) +
+                pBar25.repeat(pBar25Count) +
+                pBarHalf.repeat(pBarHalfCount) +
+                pBar75.repeat(pBar75Count) +
+                pBarZero.repeat(remaining) + "] "
+                + df.format(valuePercent) + "%";
+    }
+
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
         if (event.getMessageContent().equalsIgnoreCase(MyBot.prefix() + "sys")) {
 
             // CPU Info
-            DecimalFormat df = new DecimalFormat("#.##");
             double cpuLoad = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class).getCpuLoad();
             double cpuLoadPercent = cpuLoad * 100.0;
-
-            int fullBar = (int) cpuLoadPercent / 10;
-            int pBar25Count = 0, pBarHalfCount = 0, pBar75Count = 0;
-            double remainder = (cpuLoadPercent / 10) % 1;
-
-            // Check for "cpuLoadPercent" remainder
-            if (remainder >= .75) {
-                ++pBar75Count;
-            } else if (remainder >= .5) {
-                ++pBarHalfCount;
-            } else if (remainder >= .25) {
-                ++pBar25Count;
-            }
-
-            int remaining = 10 - fullBar - pBar25Count - pBarHalfCount - pBar75Count;
-
-            String bar = "\n[" +
-                    pBarFull.repeat(fullBar) +
-                    pBar25.repeat(pBar25Count) +
-                    pBarHalf.repeat(pBarHalfCount) +
-                    pBar75.repeat(pBar75Count) +
-                    pBarZero.repeat(remaining) + "] "
-                    + df.format(cpuLoadPercent) + "%";
 
             // Message Embed
             MessageAuthor author = event.getMessageAuthor();
             EmbedBuilder sysEmb = new EmbedBuilder()
                     .setTitle("System Information")
                     .setColor(Color.decode("0x69ff96"))
-                    .addField("Resources","* CPU Load" + bar, true)
+                    .addField("Resources", "* CPU Load" + this.visualBar(cpuLoadPercent), true)
                     .setFooter(author.getName(), author.getAvatar());
 
             event.getChannel().sendMessage(sysEmb);
