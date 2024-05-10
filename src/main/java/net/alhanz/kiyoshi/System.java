61a208f7db77b47fb66b2c21aa04f6068421e608
alhanz <alhanz1135@gmail.com>
2024-05-10 19:52:45 +0800

    Added Embed for System Command.

--- src/main/java/net/alhanz/kiyoshi/System.java
+++ src/main/java/net/alhanz/kiyoshi/System.java
@@ -1,21 +1,24 @@
 package net.alhanz.kiyoshi;
 
+import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
-
 import com.sun.management.OperatingSystemMXBean;
 
+
+import java.awt.*;
 import java.lang.management.ManagementFactory;
 import java.text.DecimalFormat;
 
 public class System implements MessageCreateListener {
 
     // Emoji List
-    String pBarZero = "<:bar_zero:1238295241740783616>";
-    String pBar25 = "<:bar_25:1238295187277615226>";
-    String pBarHalf = "<:bar_50:1238295239362482228>";
-    String pBar75 = "<:bar_75:1238295244366286909>";
-    String pBarFull = "<:bar_full:1238295236657152102>";
+    String pBarZero = "<:bar0:1238447780914266132>";
+    String pBar25 = "<:bar25:1238447791273934959>";
+    String pBarHalf = "<:bar50:1238447788526796863>";
+    String pBar75 = "<:bar75:1238447786127654942>";
+    String pBarFull = "<:bar100:1238447783514603581>";
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
@@ -41,17 +44,23 @@ public class System implements MessageCreateListener {
 
             int remaining = 10 - fullBar - pBar25Count - pBarHalfCount - pBar75Count;
 
-            String bar = "CPU Load: " + df.format(cpuLoadPercent) + "%\n" + "[" +
+            String bar = "\n[" +
                     pBarFull.repeat(fullBar) +
                     pBar25.repeat(pBar25Count) +
                     pBarHalf.repeat(pBarHalfCount) +
                     pBar75.repeat(pBar75Count) +
-                    pBarZero.repeat(remaining) +
-                    "]";
+                    pBarZero.repeat(remaining) + "] "
+                    + df.format(cpuLoadPercent) + "%";
 
-            // Bot Uptime
+            // Message Embed
+            MessageAuthor author = event.getMessageAuthor();
+            EmbedBuilder sysEmb = new EmbedBuilder()
+                    .setTitle("System Information")
+                    .setColor(Color.decode("0x69ff96"))
+                    .addField("Resources","* CPU Load" + bar, true)
+                    .setFooter(author.getName(), author.getAvatar());
 
-            event.getChannel().sendMessage(bar);
+            event.getChannel().sendMessage(sysEmb);
         }
     }
 }
